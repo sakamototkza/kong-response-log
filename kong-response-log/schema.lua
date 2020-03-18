cc3e03be80f9515352a81a71619dde77b5d7f4a4
sakamototkza <48934191+sakamototkza@users.noreply.github.com>
2020-03-18 10:50:28 +0900

    Update schema.lua

--- kong-response-log/schema.lua
+++ kong-response-log/schema.lua
@@ -2,5 +2,6 @@ return {
   no_consumer = true,
   fields = {
     test = {type = "boolean", default = false, required = "true"},
+    value = {type = "boolean", default = true, required = "false"},
   }
 }
